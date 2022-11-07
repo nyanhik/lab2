e61e398755807e03208fb8857e84d569d9d7ae32
nyanhik <rodygina.stud@yandex.ru>
2022-11-07 13:12:51 +0400

    wrap in class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 
-function Gost(encryptKey) {
+class Gost {
 
-    if (encryptKey.length != 8){
-        alert("Длина ключа должна быть 8 символов");
+    constructor(encryptKey) {
+        this.key = encryptKey
+
+        if (this.key.length != 8){
+            alert("Длина ключа должна быть 8 символов");
+        }
+
+        this.cryptoKey = this.key; //Ключ зашифрования/Расшифрования
     }
 
 
-    var cryptoKey = encryptKey; //Ключ зашифрования/Расшифрования
-
     //Таблица замен
-    var EXCHANGE_TABLE = [
+    EXCHANGE_TABLE = [
         {0: '9', 1: '6', 2: '3', 3: '2', 4: '8', 5: 'b', 6: '1', 7: '7', 8: 'a', 9: '4', a: 'e', b: 'f', c: 'c', d: '0', e: 'd', f: '5'},
         {0: '3', 1: '7', 2: 'e', 3: '9', 4: '8', 5: 'a', 6: 'f', 7: '0', 8: '5', 9: '2', a: '6', b: 'c', c: 'b', d: '4', e: 'd', f: '1'},
         {0: 'e', 1: '4', 2: '6', 3: '2', 4: 'b', 5: '3', 6: 'd', 7: '8', 8: 'c', 9: 'f', a: '5', b: 'a', c: '0', d: '7', e: '1', f: '9'},
@@ -19,7 +23,7 @@ function Gost(encryptKey) {
         {0: '1', 1: 'd', 2: '2', 3: '9', 4: '7', 5: 'a', 6: '6', 7: '0', 8: '8', 9: 'c', a: '4', b: '5', c: 'f', d: '3', e: 'b', f: 'e'},
         {0: 'b', 1: 'a', 2: 'f', 3: '5', 4: '0', 5: 'c', 6: 'e', 7: '8', 8: '6', 9: '2', a: '3', b: '9', c: '1', d: '7', e: 'd', f: '4'}
     ];
-    var MODE = {
+    MODE = {
         encrypt:    [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 0],
         decrypt:    [0, 1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1, 0, 7, 6, 5, 4, 3, 2, 1, 0, 7, 6, 5, 4, 3, 2, 1, 0],
     };
@@ -27,13 +31,13 @@ function Gost(encryptKey) {
 
      //Метод блочного шифрования в режиме простой замены
 
-    this.crypt = function (text, mode) { //входной текст + Режим работы ключа (encrypt/decrypt)
-        var xorResult = '';
-        var result = '';
+    crypt = (text, mode) => { //входной текст + Режим работы ключа (encrypt/decrypt)
+        let xorResult = '';
+        let result = '';
 
-        splitIntoBlocks(text).forEach(function (block) {
-            MODE[mode].forEach(function (k) {
-                xorResult = CicleLeftShift(exchangeChars(mod2_32(block[1].charCodeAt(0), cryptoKey.charCodeAt(k))), 11) ^ block[0].charCodeAt(0);
+        this.splitIntoBlocks(text).forEach((block) => {
+            this.MODE[mode].forEach((k) => {
+                xorResult = this.CicleLeftShift(this.exchangeChars(this.mod2_32(block[1].charCodeAt(0), this.cryptoKey.charCodeAt(k))), 11) ^ block[0].charCodeAt(0);
                 block.reverse();
                 block[1] = String.fromCharCode(xorResult);
 
@@ -47,37 +51,22 @@ function Gost(encryptKey) {
 
     };
 
-
-    this.encryptKey = function(encryptKey){
-
-        if (!arguments.length) return cryptoKey;
-        if (encryptKey.length != 8){
-
-            alert("Длина ключа должна быть 8 символов");
-        }
-        cryptoKey = encryptKey;
-    };
-
-
-
-
-
-    function padString(string, char, length) { //дописываем в начало целевой строки заданные символы до достижени нужной длины
+    padString(string, char, length) { //дописываем в начало целевой строки заданные символы до достижени нужной длины
         while (string.length < length)
             string = char + string;
         return string;
     }
 
-    function exchangeChars(input) { //разделение 32-х битного блока на 4-х битные блоки +  заменяем по таблице
-        return parseInt(padString(input.toString(2), '0', 32).replace(/(....)/g, function (match) {
+    exchangeChars = (input) => { //разделение 32-х битного блока на 4-х битные блоки +  заменяем по таблице
+        return parseInt(this.padString(input.toString(2), '0', 32).replace(/(....)/g, function (match) {
             return parseInt(match, 2).toString(16);
-        }).split('').reverse().map(function (item, i) {
-            return EXCHANGE_TABLE[i][item];
+        }).split('').reverse().map((item, i) => {
+            return this.EXCHANGE_TABLE[i][item];
         }).reverse().join(''), 16);
 
     }
 
-    function splitIntoBlocks(text) { //разделяет входящую строку на блоки по 2 символа (32*2 = 64 бит)
+    splitIntoBlocks(text) { //разделяет входящую строку на блоки по 2 символа (32*2 = 64 бит)
 
         if (text.length % 2) {
           text += ' '; //есл в строке нечетное количетсов символов, то дописывает в конец пробел
@@ -87,14 +76,14 @@ function Gost(encryptKey) {
 
     }
 
-    function CicleLeftShift(value, shift) { //Циклический сдвиг влево
+    CicleLeftShift(value, shift) { //Циклический сдвиг влево
 
         return parseInt(parseInt(value).toString(2).slice(shift) + parseInt(value).toString(2).slice(0, shift), 2);
 
     }
 
-    function mod2_32(a, b) { //сложение по модулю 32
-        var result = (a + b).toString(2);
+    mod2_32 = (a, b) => { //сложение по модулю 32
+        let result = (a + b).toString(2);
 
         return result.length > 32 ? parseInt(result.slice(1), 2) : parseInt(result, 2);
     }
@@ -104,23 +93,23 @@ function Gost(encryptKey) {
 
 
 function encryptMessage() {
-    var enterText = document.getElementById("text-to-convert").value;
+    let enterText = document.getElementById("text-to-convert").value;
   	console.log(enterText);
-  	var key = document.getElementById("key").value;
+  	let key = document.getElementById("key").value;
   	console.log(key);
-  	var EncryptedMessage = new Gost(key);
-  	var encryptMessage = EncryptedMessage.crypt(enterText,'encrypt');
+  	let EncryptedMessage = new Gost(key);
+  	let encryptMessage = EncryptedMessage.crypt(enterText,'encrypt');
 
   	document.getElementById("encrypt-message").value = encryptMessage;
 }
 
 function decryptMessage() {
-  	var encryptMessage = document.getElementById("encrypt-message").value;
+  	let encryptMessage = document.getElementById("encrypt-message").value;
   	console.log(encryptMessage);
-  	var key = document.getElementById("key").value;
+  	let key = document.getElementById("key").value;
   	console.log(key);
-  	var EncryptedMessage = new Gost(key);
-  	var enterText = EncryptedMessage.crypt(encryptMessage,'decrypt');
+  	let EncryptedMessage = new Gost(key);
+  	let enterText = EncryptedMessage.crypt(encryptMessage,'decrypt');
 
   	document.getElementById("text-to-convert").value = enterText;
 }
